d8e54183abf3a704e507f2062987c05daed9a07f
GitKimUser <kmg6488@gmail.com>
2026-05-15 16:41:43 +0900

    feat:회원 repository의 조회 및 중복 검사 메서드 추가

--- src/main/java/com/alvibe/qna/repository/MemberRepository.java
+++ src/main/java/com/alvibe/qna/repository/MemberRepository.java
@@ -1,4 +1,12 @@
 package com.alvibe.qna.repository;
 
-public class MemberRepository {
+import com.alvibe.qna.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+    boolean existsByEmail(String email);
+    boolean existsByNickname(String nickname);
 }
