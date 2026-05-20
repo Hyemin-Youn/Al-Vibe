dfa67570628866021483d71662d5f8c32d210044
parksamryeong <shyp476@naver.com>
2026-05-20 10:12:54 +0900

    feat:  마이페이지에서 사용할 답변 목록, 답변 수, 채택된 답변 수 메서드 추가
     // 회원이 작성한 답변 목록
        List<Answer> findByMemberIdAndIsDeletedFalseAndParentIsNull(Long memberId);
    
        // 회원이 작성한 답변 수
        int countByMemberIdAndIsDeletedFalseAndParentIsNull(Long memberId);
    
        // 회원이 채택된 답변 수
        int countByMemberIdAndIsSelectedTrueAndIsDeletedFalse(Long memberId);

--- src/main/java/com/alvibe/qna/repository/AnswerRepository.java
+++ src/main/java/com/alvibe/qna/repository/AnswerRepository.java
@@ -4,6 +4,16 @@ import com.alvibe.qna.entity.Answer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AnswerRepository extends JpaRepository<Answer, Long>, AnswerRepositoryCustom {
+    // 회원이 작성한 답변 목록
+    List<Answer> findByMemberIdAndIsDeletedFalseAndParentIsNull(Long memberId);
+
+    // 회원이 작성한 답변 수
+    int countByMemberIdAndIsDeletedFalseAndParentIsNull(Long memberId);
+
+    // 회원이 채택된 답변 수
+    int countByMemberIdAndIsSelectedTrueAndIsDeletedFalse(Long memberId);
 }
