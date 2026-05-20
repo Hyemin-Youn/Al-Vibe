1176dcd4a15554a141c02fac558642d5aff11c0c
linnbb <zhaltltm@gmail.com>
2026-05-20 10:55:46 +0900

    [feat].질문글 신고 기능 추가
    [feat].질문글 신고 기능 추가

--- src/main/java/com/alvibe/qna/repository/ReportRepository.java
+++ src/main/java/com/alvibe/qna/repository/ReportRepository.java
@@ -9,4 +9,7 @@ public interface ReportRepository extends JpaRepository<Report, Long> {
 
     // 중복 신고 방지
     boolean existsByReporterIdAndTargetAnswerId(Long reporterId, Long answerId);
+
+    // 질문 중복 신고 방지
+    boolean existsByReporterIdAndTargetQuestionId(Long reporterId, Long questionId);
 }
