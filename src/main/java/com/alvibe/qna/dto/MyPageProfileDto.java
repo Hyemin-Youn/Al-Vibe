d0c44e91030e3ee6e91c4725319ed10166330d35
GitKimUser <kmg6488@gmail.com>
2026-05-20 13:23:17 +0900

    마이페이지 질문, 답변 count 반영 및 목록 반영

--- src/main/java/com/alvibe/qna/dto/MyPageProfileDto.java
+++ src/main/java/com/alvibe/qna/dto/MyPageProfileDto.java
@@ -1,13 +1,14 @@
 package com.alvibe.qna.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
-@AllArgsConstructor
+@Setter
 public class MyPageProfileDto { // 프로필 카드 영역 데이터
+    private Long Id;
     private String nickname;
     private String email;
     private LocalDateTime createAt;
