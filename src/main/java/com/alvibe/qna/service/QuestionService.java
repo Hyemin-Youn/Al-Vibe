57e6bd099ba9baf7d278657cd76bba087fdc3a01
linnbb <zhaltltm@gmail.com>
2026-05-20 10:02:54 +0900

    [fix].myfage용 질문글 가져오기 메서드 추가
    [fix].myfage용 질문글 가져오기 메서드 추가

--- src/main/java/com/alvibe/qna/service/QuestionService.java
+++ src/main/java/com/alvibe/qna/service/QuestionService.java
@@ -179,4 +179,15 @@ public class QuestionService {
 //        // like 칼럼 없음
 //        return questionRepository.findTop5ByOrderByLikeCountDesc();
 //    }
+
+
+    // 회원의 질문 개수 조회
+    public long countQuestionsByMember(Long memberId) {
+        return questionRepository.countByMember_IdAndIsDeletedFalse(memberId);
+    }
+
+    // 회원의 질문 목록 조회
+    public List<Question> getQuestionsByMember(Long memberId) {
+        return questionRepository.findByMember_IdAndIsDeletedFalseOrderByCreatedAtDesc(memberId);
+    }
 }
